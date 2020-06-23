00940eb72cff494db8c9f0d4640d56195869e2b0
Chubbray <chubbray1@gmail.com>
2020-06-23 17:51:21 -0600

    getting an error

---
+++ app.js
@@ -0,0 +1,33 @@
+// console.log('Hello Node! ');
+// console.log(document);
+
+// var message = 'Hello Node!';
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const printProfileData = (profileDataArr) => {
+//     console.log(profileDataArr);
+// };
+
+//printProfileData(profileDataArgs);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+};
+
+printProfileData();
